90296dc88864a3db6baeb382248a7a4fc7ec5ef6
Glenford Williams <gw_dev@outlook.com>
2020-09-05 07:22:02 -0500

    make sure not to reinitialize player

--- src/components/video-player/index.js
+++ src/components/video-player/index.js
@@ -5,48 +5,48 @@ import { setUpVideoControls } from './helpers/setUpVideoControls';
 import 'video.js/dist/video-js.css';
 import { useState, useEffect, useRef } from 'react';
 
-const playerData = [
-  { key: "key1", val: "val1" },
-  { key: "key2", val: "val2" },
-  { key: "key3", val: "val3" },
-]
+
 
 const UPlayVideoPlayer = ({ src, banner = [] }) => {
   const videoPlayerRef = useRef(null); // Instead of ID
   const [currentTime, setCurrentTime] = useState(null);
-  const videoJSOptions = {
-    controls: true,
-    responsive: true,
-    aspectRatio: '4:2',
-    userActions: { hotkeys: true },
-    inactivityTimeout : 1000,
-    playbackRates: [0.5, 1, 1.5, 2]
-  };
+  const [initializedPlayer, setInitializedPlayer] = useState(false);
 
   useEffect(() => {
+
+    const videoJSOptions = {
+      controls: true,
+      responsive: true,
+      aspectRatio: '4:2',
+      userActions: { hotkeys: true },
+      inactivityTimeout: 1000,
+      playbackRates: [0.5, 1, 1.5, 2]
+    };
+
     if (videoPlayerRef) {
-      const player = videojs(videoPlayerRef.current, videoJSOptions, () => {
+      if (!initializedPlayer) {
+        const player = videojs(videoPlayerRef.current, videoJSOptions, () => {
 
-        player.on("ended", () => {
-          console.log("ended");
+          player.on("ended", () => {
+            console.log("ended");
+          });
+
+          player.on("timeupdate", () => {
+            setCurrentTime(player.currentTime());
+          });
+
+          setUpVideoControls(player, {
+            items: banner
+          });
+
+          player.src(src);
+          setInitializedPlayer(true);
         });
-
-        player.on("timeupdate", () => {
-          setCurrentTime(player.currentTime());
-        });
-
-        setUpVideoControls(player, {
-          items: banner
-        });
-
-        player.src(src);
-
-      });
-
+      }
     }
 
     return () => { };
-  }, [banner]);
+  }, [banner, src, initializedPlayer]);
 
   return (
     <div>
